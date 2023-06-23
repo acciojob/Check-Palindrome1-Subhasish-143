efd4fe8cdecc1c0ba0122d48c894bf11eb9ca68f
Subhasish Padhee <44895248+Subhasish-143@users.noreply.github.com>
2023-06-23 10:31:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,7 @@
 function palindrome(str){
 	let l = 0, r = str.length-1;
 	while(l < r) {
-		let s = str.charAt(l)-'a' >= 0 && str.charAt(l)-'a' <= 25 ? str.charAt(l)-'a' : str.charAt(l)-'A';
-		let b = str.charAt(r)-'a' >= 0 && str.charAt(r)-'a' <= 25 ? str.charAt(r)-'a' : str.charAt(r)-'A';
-		
-		if (s!=b) {
+		if (str.charCodeAt(l)!=str.charCodeAt(r)) {
 			return false;
 		}
 		l++;
